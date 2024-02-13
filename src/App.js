08c8d4054c37e65d9eee01df47874f5b49faa22f
Niket <niketj2000@gmail.com>
2024-02-13 17:57:45 +0530

    📦 NEW: Added different tabs in header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import "./styles/main.css";
 import { useState } from "react";
+import Hero from "./components/Hero";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -9,7 +10,9 @@ function App() {
     <div className={darkMode && "dark"}>
       <Header darkMode={darkMode} setDarkMode={setDarkMode} />
 
-      <main className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white"></main>
+      <main className="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
+        <Hero />
+      </main>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,21 @@ import { MdNightsStay, MdWbSunny } from "react-icons/md";
 
 const Header = ({ darkMode, setDarkMode }) => {
   return (
-    <header className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white border-b border-gray-300">
+    <header className="bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-white px-15 h-16 sticky top-0">
       <nav className="flex justify-between items-center p-5">
         <h1 className="text-lg">Niket Jain</h1>
-
+        <div className="ml-auto mx-8 hover:border border-gray-400 px-4 rounded-lg hover:cursor-pointer">
+          Home
+        </div>
+        <div className="mx-8 hover:border border-gray-400 px-4 rounded-lg hover:cursor-pointer">
+          About
+        </div>
+        <div className="mx-8 hover:border border-gray-400 px-4 rounded-lg hover:cursor-pointer">
+          Projects
+        </div>
+        <div className="mx-8 hover:border border-gray-400 px-4 rounded-lg hover:cursor-pointer">
+          Contact
+        </div>
         <div onClick={() => setDarkMode(!darkMode)}>
           {darkMode ? (
             <MdWbSunny className="body-base-medium cursor-pointer" />
