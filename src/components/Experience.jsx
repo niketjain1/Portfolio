a139f309d45651b557f08880ac315a45e55a4141
Niket <niketj2000@gmail.com>
2024-02-14 18:49:30 +0530

    👌 IMPROVE: change the background color for dark mode

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,22 +4,24 @@ import NumansLogo from "../assets/NumansLogo.svg";
 const Experience = () => {
   return (
     <div className="flex flex-col px-56">
-      <div className=" border shadow-sm p-8">
-        <h2 className="title-base mb-8 bg-gradient-to-b from-blue-300 to-blue-600 bg-clip-text text-transparent">
-          Experience
-        </h2>
+      <h2 className="title-base mb-8 bg-gradient-to-b from-purple-300 to-purple-600 bg-clip-text text-transparent">
+        Experience
+      </h2>
+      <div className="border-gray-900 bg-gradient-to-b from-gray-200 to-gray-100 dark:bg-gradient-to-b dark:from-purple-700 dark:to-purple-300 shadow-sm p-8 rounded-md">
         <div className="flex items-center mb-2">
           <img src={NumansLogo} alt="NumansLogo" />
-          <div className="subtitle-base ml-5 text-gray-800 dark:text-white">
+          <div className="subtitle-base ml-5 text-gray-800 dark:text-gray-100">
             Software Developer Intern at Numans
           </div>
-          <div className="ml-auto text-gray-600">June 2021 - August 2021</div>
+          <div className="ml-auto text-gray-800 dark:text-gray-100">
+            June 2021 - August 2021
+          </div>
         </div>
         <div className="mb-3 text-start">
-          <strong className="text-blue-600 dark:text-blue-300">
+          <strong className="text-purple-600 dark:text-white underline">
             Core Development on Device History Feature
           </strong>
-          <ul className="list-disc list-inside">
+          <ul className="list-disc list-inside font-fakt">
             <li>
               Designed and implemented a comprehensive 'Device History' feature
               to streamline device management for HR and IT teams.
@@ -33,10 +35,10 @@ const Experience = () => {
           </ul>
         </div>
         <div className="mb-3 text-start">
-          <strong className="text-blue-600 dark:text-blue-300">
+          <strong className="text-purple-600 dark:text-white underline">
             Automated Notification System
           </strong>
-          <ul className="list-disc list-inside">
+          <ul className="list-disc list-inside font-fakt">
             <li>
               Engineered a streamlined Slack notification system using AWS
               Lambda, reducing Turnaround Time (TAT) by an impressive 30%.
@@ -48,10 +50,10 @@ const Experience = () => {
           </ul>
         </div>
         <div className="mb-3 text-start">
-          <strong className="text-blue-600 dark:text-blue-300">
+          <strong className="text-purple-600 dark:text-white underline">
             Performance Optimization Initiatives
           </strong>
-          <ul className="list-disc list-inside">
+          <ul className="list-disc list-inside font-fakt">
             <li>
               Led efforts to boost product performance, achieving a 25%
               reduction in data retrieval times. Implemented the refinement of
