b1da1ab83805fc791f1fac2731180f5f68438535
Niket <niketj2000@gmail.com>
2024-02-14 10:47:56 +0530

    👌 IMPROVE: Added setSection state to scrolling the section into view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,41 @@
 import { MdNightsStay, MdWbSunny } from "react-icons/md";
 
-const Header = ({ darkMode, setDarkMode }) => {
+const Header = ({ darkMode, setDarkMode, setSection }) => {
   return (
-    <header className="bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-white px-15 h-16 sticky top-0">
+    <header className="bg-gray-200 dark:bg-gray-600 text-gray-900 dark:text-white px-15 h-16 sticky top-0 z-50 shadow-xs">
       <nav className="flex justify-between items-center p-5">
         <h1 className="text-lg">Niket Jain</h1>
-        <div className="ml-auto mx-8 hover:border border-gray-400 px-4 rounded-lg hover:cursor-pointer">
+        <button
+          className="ml-auto mx-8 hover:border border-gray-400 px-4 rounded-lg"
+          onClick={() => setSection("Home")}
+        >
           Home
-        </div>
-        <div className="mx-8 hover:border border-gray-400 px-4 rounded-lg hover:cursor-pointer">
-          About
-        </div>
-        <div className="mx-8 hover:border border-gray-400 px-4 rounded-lg hover:cursor-pointer">
+        </button>
+        <button
+          className="mx-8 hover:border border-gray-400 px-4 rounded-lg hover:shadow-xs"
+          onClick={() => setSection("Projects")}
+        >
           Projects
-        </div>
-        <div className="mx-8 hover:border border-gray-400 px-4 rounded-lg hover:cursor-pointer">
+        </button>
+        <button
+          className="mx-8 hover:border border-gray-400 px-4 rounded-lg hover:shadow-xs"
+          onClick={() => setSection("Experience")}
+        >
+          Experience
+        </button>
+        <button
+          className="mx-8 hover:border border-gray-400 px-4 rounded-lg hover:shadow-xs"
+          onClick={() => setSection("Contact")}
+        >
           Contact
-        </div>
-        <div onClick={() => setDarkMode(!darkMode)}>
+        </button>
+        <button onClick={() => setDarkMode(!darkMode)}>
           {darkMode ? (
             <MdWbSunny className="body-base-medium cursor-pointer" />
           ) : (
             <MdNightsStay className="body-base-medium cursor-pointer" />
           )}
-        </div>
+        </button>
       </nav>
     </header>
   );
