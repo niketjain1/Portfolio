475f633b95e8b24f47b2c4748c39d8a6f95796fb
Niket <niketj2000@gmail.com>
2024-02-15 21:50:38 +0530

    📦 NEW: Added logo in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { MdNightsStay, MdWbSunny } from "react-icons/md";
+import logo from "../assets/logo.jpg";
 
 const Header = ({ darkMode, setDarkMode, setSection }) => {
   return (
     <header className="bg-gray-200 dark:bg-gray-600 text-gray-900 dark:text-white px-15 h-18 sticky top-0 z-50 shadow-xs">
       <nav className="flex justify-between items-center p-5">
-        <h1 className="text-lg">Niket Jain</h1>
+        <img src={logo} alt="logo" className="w-10 h-10 rounded-full" />
         <button
           className="ml-auto mx-8 px-4 rounded-lg hover:bg-gray-50 py-2 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white hover:shadow-xs"
           onClick={() => setSection("Home")}
