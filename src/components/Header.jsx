fe0005f510c5ddbd61b1a27b1edac22f138cc475
Niket <niketj2000@gmail.com>
2024-02-19 20:08:13 +0530

    👌 IMPROVE: change the text size for mobile view on the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,10 +22,10 @@ const Header = ({ darkMode, setDarkMode, setSection, isMobileScreen }) => {
             <div className="text-right ml-auto mr-4">
               <Menu as="div" className="relative inline-block text-left">
                 <div>
-                  <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+                  <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
                     {selectedSection}
                     <IoIosArrowDown
-                      className="-mr-1 ml-2 h-5 w-5 text-violet-200 hover:text-violet-100"
+                      className="-mr-1 ml-2 mt-1 h-5 w-5 text-violet-200 hover:text-violet-100"
                       aria-hidden="true"
                     />
                   </Menu.Button>
@@ -40,7 +40,7 @@ const Header = ({ darkMode, setDarkMode, setSection, isMobileScreen }) => {
                   leaveTo="transform opacity-0 scale-95"
                 >
                   <Menu.Items className="absolute left-1/2 -translate-x-1/2 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-                    <div className="px-1 py-1 ">
+                    <div className="px-1 py-2 ">
                       <Menu.Item>
                         <button
                           className="text-gray-900 group flex w-full items-center rounded-md px-2 py-2 text-sm"
