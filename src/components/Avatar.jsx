c41258de9745d3658522473e09214a21791beedd
Niket <niketj2000@gmail.com>
2024-02-26 16:34:07 +0530

    🐛 FIX: animation for avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -5,16 +5,30 @@ import profilePic from "../assets/profilePic.jpg";
 const Avatar = () => {
   const [isHovered, setIsHovered] = useState(false);
 
+  const handleMouseEnter = () => {
+    if (!isHovered) {
+      setIsHovered(true);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (isHovered) {
+      setIsHovered(false);
+    }
+  };
+
   return (
     <div
-      className={`py-3 mt-5 ${isHovered ? "hovered" : ""}`}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+      className="py-3 mt-5"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <img
         src={isHovered ? profilePic : avatar}
         alt="avatar"
-        className="w-60 h-60 md:w-60 md:h-60 object-cover object-top bg-gradient-to-br from-red-400 to-purple-500 rounded-full"
+        className={`w-60 h-60 md:w-60 md:h-60 object-cover object-top bg-gradient-to-br from-red-400 to-purple-500 rounded-full ${
+          isHovered ? "hovered" : ""
+        }`}
       />
     </div>
   );
